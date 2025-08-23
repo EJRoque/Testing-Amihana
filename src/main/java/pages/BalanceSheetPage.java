fcb8eebdd90dda45e70d68438ebe6b1a5e68567c
EJRoque <roque.ernestjohn.s.5002@gmail.com>
2025-08-23 22:33:51 +0800

    Balance sheet wip

--- src/main/java/pages/BalanceSheetPage.java
+++ src/main/java/pages/BalanceSheetPage.java
@@ -24,18 +24,58 @@ public class BalanceSheetPage {
 	
 	@FindBy(className = "ant-modal-close-x")
 	WebElement closeModalBtn;
-//	
+	
+	@FindBy(xpath = "//button//span[text()='View Amount']")
+	WebElement viewAmountBtn;
+	
+	@FindBy(xpath = "//button[img[@alt='Close Icon']]")
+	WebElement viewAmountCloseBtn;
+	
+	@FindBy(xpath = "//button[@type='button' and span[text()='Edit']]")
+	WebElement editBtn;
+	
+	@FindBy(xpath = "//button[@type='button' and span[normalize-space()='Add New User']]")
+	WebElement addNewUserBtn;
+	
+	@FindBy(xpath = "//div[@class='ant-transfer-list-header']//input[@type='checkbox']")
+	WebElement selectAllUser;
+	
+	@FindBy(xpath = "//button[@type='button']//span[@aria-label='right']")
+	WebElement transferUser;
+	
+	@FindBy(xpath = "//button[@type='button']//span[text()='Add Users']")
+	WebElement addUsersBtn;
+	
+	@FindBy(xpath = "//button[@type='button']//span[text()='Adjust Amount']")
+	WebElement adjustAmountBtn;
+	
+	@FindBy(xpath = "//label[text()='Jan']/following-sibling::input[@type='number']")
+	WebElement janNumberBox;
+	
+	@FindBy(xpath = "//label[text()='Feb']/following-sibling::input[@type='number']")
+	WebElement febNumberBox;
+	
+	@FindBy(xpath = "//label[text()='Mar']/following-sibling::input[@type='number']")
+	WebElement marNumberBox;
+	
+	@FindBy(xpath = "//label[text()='HOA Membership']/following-sibling::input[@type='number']")
+	WebElement hoaNumberBox;
+	
+	@FindBy(xpath = "//button[@type='button']//span[text()='Save Monthly Amounts']")
+	WebElement saveMonthlyAmountBtn;
+	
+	@FindBy(xpath = "//button[@type='button']//span[text()='Save']")
+	WebElement saveBtn;
+	
 //	@FindBy(xpath = "")
 //	WebElement
-//	
+	
 //	@FindBy(xpath = "")
 //	WebElement
-//	
+	
 //	@FindBy(xpath = "")
 //	WebElement
-//	
-//	@FindBy(xpath = "")
-//	WebElement
+	
 //	@FindBy(xpath = "")
 //	WebElement
 	
@@ -57,8 +97,40 @@ public class BalanceSheetPage {
 		Log.info("Venue modal closed...");
 	}
 	
-	public void enterNewYear(String year) {
+	public void enterNewYear(int year) {
 		addNewYearBtn.click();
-		yearTextBox.sendKeys(year, Keys.ENTER);
+		yearTextBox.clear();
+		yearTextBox.sendKeys(String.valueOf(year + 1), Keys.ENTER);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		Log.info("New year entered...");
+	}
+	
+	public void clickEditButton() {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		editBtn.click();
+		Log.info("Edit button clicked...");
+	}
+	
+	public void clickAddNewUserButton() {
+		addNewUserBtn.click();
+		selectAllUser.click();
+		transferUser.click();
+		addUsersBtn.click();
+		Log.info("Add new users button clicked...");
+	}
+	
+	public void clickAdjustAmountButton(String jan, String feb, String mar, String hoa) {
+		adjustAmountBtn.click();
+		janNumberBox.sendKeys(jan);
+		febNumberBox.sendKeys(feb);
+		marNumberBox.sendKeys(mar);
+		hoaNumberBox.sendKeys(hoa);
+		saveMonthlyAmountBtn.click();
+		Log.info("Monthly amount adjusted...");
+	}
+	
+	public void clickSaveButton() {
+		saveBtn.click();
+		Log.info("Save button clicked...");
 	}
 }
--- src/test/java/tests/BalanceSheetTest.java
+++ src/test/java/tests/BalanceSheetTest.java
@@ -22,4 +22,22 @@ public class BalanceSheetTest extends BaseTest{
 		test.info("venue modal closed");
 		test.pass("Modal Opening and Closing test succesfully");
 	}
+	
+	@Test
+	public void testCreatingNewBalanceSheet() {
+		test = ExtentReportManager.createTest("Test Creating New Balance Sheet");
+		BalanceSheetPage balanceSheetPage = new BalanceSheetPage(driver);
+		
+		balanceSheetPage.enterNewYear(2040);
+		test.info("New year entered");
+		balanceSheetPage.clickEditButton();
+		test.info("Edit button clicked");
+		balanceSheetPage.clickAddNewUserButton();
+		test.info("New users added");
+		balanceSheetPage.clickAdjustAmountButton("200", "200", "200", "400");
+		test.info("Monthly amount adjusted");
+		balanceSheetPage.clickSaveButton();
+		test.info("Save button clicked");
+		test.pass("New balance sheet created succesfully");
+	}
 }
